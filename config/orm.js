20e187ccd3feace7ce4e6aa14702bb38ece84de1
Mateo Vargas <mateovargas@Mateos-MBP.san.rr.com>
2018-02-26 14:23:50 -0800

    Finished project

--- config/orm.js
+++ config/orm.js
@@ -10,22 +10,40 @@ function printQuestionMarks(length){
 
     return questionMarks.toString();
 }
+
+function objectToSql(ob){
+    var array = [];
+    for(var key in ob){
+        var value = ob[key];
+        console.log(value);
+
+        if(Object.hasOwnProperty.call(ob, key)){
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'"; 
+            }
+
+            array.push(key + "=" + value);
+        }
+    }
+
+    return array.toString();
+}
 var orm = {
-    selectAll: function(tableInput, callBack){
+    selectAll: function(table, callBack){
         var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [tableInput], function(err, result){
+        console.log(queryString);
+        connection.query(queryString, [table], function(err, result){
             if(err) throw err;
-            console.log(result);
             callBack(result);
         });
     },
-    insertOne: function(tableInput, cols, vals, callBack){
-        var queryString = "INSERT INTO" + table;
+    insertOne: function(table, cols, vals, callBack){
+        var queryString = "INSERT INTO " + table;
 
         queryString = queryString +  " (" + cols.toString() + ") ";
         queryString = queryString + "VALUES (" + printQuestionMarks(vals.length) + ") ";
 
-        console.log(quertyString);
+        console.log(queryString);
 
         connection.query(queryString, vals, function(err, result){
             if(err) throw err;
@@ -33,10 +51,39 @@ var orm = {
         });
         
     },
-    delete: function(tableInput, condition, callBack)
+    update: function(table, objColVals, condition, callback){
+        console.log(table);
+        var queryString = "UPDATE " + table;
 
+        queryString = queryString + " SET ";
+        queryString = queryString + objectToSql(objColVals);
+        queryString = queryString + " WHERE ";
+        queryString = queryString + condition;
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+            if(err){
+                throw err;
+            }
 
+            callback(result);
+        });
+    },
+    delete: function(table, condition, callBack){
+      var queryString = "DELETE FROM " + table;
+
+      queryString = queryString + " WHERE ";
+      queryString = queryString + condition;
+      console.log(queryString);
+      connection.query(queryString, function(err, result){
+        if(err){
+            throw err;
+        }
+        callBack(result);
+      });
+    }
 };
 
+
+module.exports = orm;
 
 
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var routes = require('./controller/burger_controller.js');
 
+var PORT = process.env.PORT || 8080;
 var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("App now listening at localhost:" + PORT);
+});
